339db9a78e7520627ed928282380cd65c48ee269
Ali Almasi <a710almasi@gmail.com>
2025-07-30 14:19:40 +0330

    update: version 1.0.1

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,31 @@
 import { Command } from "commander";
 import chalk from "chalk";
 import ora from "ora";
-import boxen from "boxen";
+import boxen, { Options } from "boxen";
+import * as fs from "node:fs";
+import { colorize } from "json-colorizer";
+import type { AnswerObject } from "./types.js";
+
+const projectFile = JSON.parse(fs.readFileSync("./package.json").toString());
 
 const program = new Command();
 program
-	.name("8-magic")
-	.description("A clairvoyant piece of software, in your CLI")
-	.version("1.0.0");
-// .option(
-// 	"-t, --type < all | positive | neutral | negative ",
-// 	"Type of answer you want",
-// 	"all"
-// );
+	.name(projectFile.name)
+	.description(projectFile.description)
+	.version(projectFile.version)
+	.option("--json", "Receive raw JSON output from the API");
 
 program.parse(process.argv);
 
-// const options = program.opts();
+const options = program.opts();
+
+const boxenSettings: Options = {
+	padding: 1,
+	margin: 1,
+	borderColor: "green",
+	borderStyle: "round",
+	textAlignment: "left"
+};
 
 const emoji = (type: string): string => {
 	switch (type) {
@@ -33,10 +42,37 @@ const emoji = (type: string): string => {
 	}
 };
 
+function printAnswer({ answer, type }: AnswerObject): string {
+	switch (type) {
+		case "positive":
+			return chalk.greenBright(answer);
+		case "neutral":
+			return chalk.yellowBright(answer);
+		case "negative":
+			return chalk.redBright(answer);
+		default:
+			return chalk.white(answer);
+	}
+}
+
+function printType(type: string): string {
+	switch (type) {
+		case "positive":
+			return "YES";
+		case "neutral":
+			return "IDK";
+		case "negative":
+			return "NO";
+		default:
+			return "";
+	}
+}
+
 (async () => {
 	const spinner = ora(`Hold up, consulting the universe ⊹₊⟡⋆`).start();
 	spinner.color = "green";
 	spinner.spinner = "star";
+	spinner.indent = 1;
 
 	try {
 		const res = await fetch(`https://api.8.alialmasi.ir/v2/answers`);
@@ -47,24 +83,25 @@ const emoji = (type: string): string => {
 		}
 
 		const data = await res.json();
+		const answerObject: AnswerObject = data?.data.answer;
 		spinner.succeed("The universe delivers. Check it out ── .✦");
-
-		const boxContent = boxen(
-			chalk.greenBright(
-				`${emoji(data?.data.answer.type)} ${data?.data.answer.answer}`
-			),
-			{
-				padding: 1,
-				borderColor: "green",
-				borderStyle: "singleDouble",
-				textAlignment: "center"
-			}
-		);
+		let boxContent;
+		if (options.json)
+			boxContent = boxen(`${colorize(data, { indent: 1 })}`, boxenSettings);
+		else
+			boxContent = boxen(
+				[
+					`${emoji(answerObject.type)} ${printAnswer(answerObject)}`,
+					"",
+					`Short: ${chalk.bold(printType(answerObject.type))}`
+				].join("\n"),
+				boxenSettings
+			);
 
 		console.log(boxContent);
 	} catch (err: any) {
-		spinner.fail("Universe's servers were down:");
-		console.error(chalk.red(`💥 ${err.message}`));
+		spinner.fail("Error while contacting universe's servers:");
+		console.error(chalk.redBright(`💥 ${err.message}`));
 		process.exit(1);
 	}
 })();
